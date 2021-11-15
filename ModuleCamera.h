ae812151c9b2a809d3ba254ca1cce267309cfc84
Eric Torres Perramon <erictoper@gmail.com>
2021-11-15 21:16:05 +0100

    [ADD] New module camera

---
+++ ModuleCamera.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Module.h"
+
+class ModuleCamera :
+    public Module
+{
+    public:
+        ModuleCamera();
+        ~ModuleCamera();
+        /*
+        void setFov();
+        void SetAspectRatio();
+        void GetProjectionMatrix();
+        void GetViewMatrix()
+        */
+};
+
