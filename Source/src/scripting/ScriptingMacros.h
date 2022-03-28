9886acaba7e90839fbf54c88edb17937a7a54bf3
Baran Surucu <baransrc@gmail.com>
2022-03-28 03:44:00 +0200

    Added scripting macros.

---
+++ Source/src/scripting/ScriptingMacros.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define SERIALIZE_FIELD(type, name) type name
+
+#define SERIALIZATION_METHODS_true                                            \
+public:                                                                       \
+    virtual void SerializeTo(                                                 \
+        std::unordered_map<std::string, SerializedField>& serialized_fields   \
+    ) override;                                                               \
+    virtual void DeserializeFrom(                                             \
+        std::unordered_map<std::string, SerializedField>& serialized_fields   \
+    ) override;
+#define SERIALIZATION_METHODS_false                                           \
+public:                                                                       \
+    void SerializeTo(                                                         \
+        std::unordered_map<std::string, SerializedField>& serialized_fields   \
+    ) override;                                                               \
+    void DeserializeFrom(                                                     \
+        std::unordered_map<std::string, SerializedField>& serialized_fields   \
+    ) override;
+
+// This needs to be added to all script sub classes:
+#define SERIALIZATION_METHODS(is_virtual) SERIALIZATION_METHODS_##is_virtual
