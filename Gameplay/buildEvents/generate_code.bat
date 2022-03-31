b630bdb82b84c0b4dcd0852642da668152658774
Baran Surucu <baransrc@gmail.com>
2022-03-31 18:04:13 +0200

    Temporarily deactivated generate_code.bat

--- Gameplay/buildEvents/generate_code.bat
+++ Gameplay/buildEvents/generate_code.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0" 
 set current_dir=%cd%
-py.exe %current_dir%\run_code_generation.py
+@REM py.exe %current_dir%\run_code_generation.py
 @REM py.exe %current_dir%\run_auto_dll_versioning.py
 popd
