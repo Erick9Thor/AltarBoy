4e9c51e7bbb82ef161bd4593623b03194aa1bdb0
Bernat <bolicubo@gmail.com>
2022-04-06 18:24:33 +0200

    Added game state events for my man baran

---
+++ Source/src/events/GameStateEventPayload.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "MathGeolib.h"
+
+namespace Hachiko
+{
+    struct GameStateEventPayload
+    {
+        enum State
+        {
+            STARTED,
+            RESUMED,
+            PAUSED,
+            STOPPED            
+        };
+
+        GameStateEventPayload(State state) : state(state) {}
+
+        [[nodiscard]] State GetState() const
+        {
+            return state;
+        }
+    private:
+        State state;
+    };
+} // namespace Hachiko
