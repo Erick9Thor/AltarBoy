1e718c7a253b8856b394381866b5042f1d5813d5
Baran Surucu <baransrc@gmail.com>
2022-04-14 01:41:12 +0200

    Deleted unnecessary scripts.

--- Gameplay/generated/Factory.cpp
+++ Gameplay/generated/Factory.cpp
@@ -2,12 +2,9 @@
 #include "generated/Factory.h"
 #include "BackToMainMenu.h"
 #include "DynamicCamera.h"
-#include "Experiment.h"
-#include "Funky.h"
 #include "MainMenuManager.h"
 #include "PlayerCamera.h"
 #include "PlayerController.h"
-#include "SomeScript.h"
 
 
 Hachiko::Scripting::Script* InstantiateScript(Hachiko::GameObject* script_owner, const std::string& script_name)
@@ -20,16 +17,6 @@ Hachiko::Scripting::Script* InstantiateScript(Hachiko::GameObject* script_owner,
 	if (script_name == "DynamicCamera")
 	{
 		return new Hachiko::Scripting::DynamicCamera(script_owner);
-	}
-
-	if (script_name == "Experiment")
-	{
-		return new Hachiko::Scripting::Experiment(script_owner);
-	}
-
-	if (script_name == "Funky")
-	{
-		return new Hachiko::Scripting::Funky(script_owner);
 	}
 
 	if (script_name == "MainMenuManager")
@@ -45,11 +32,6 @@ Hachiko::Scripting::Script* InstantiateScript(Hachiko::GameObject* script_owner,
 	if (script_name == "PlayerController")
 	{
 		return new Hachiko::Scripting::PlayerController(script_owner);
-	}
-
-	if (script_name == "SomeScript")
-	{
-		return new Hachiko::Scripting::SomeScript(script_owner);
 	}
 
 	return nullptr;
--- Gameplay/generated/SerializationMethods.cpp
+++ Gameplay/generated/SerializationMethods.cpp
@@ -4,12 +4,9 @@
 #include "scriptingUtil/gameplaypch.h"
 #include "BackToMainMenu.h"
 #include "DynamicCamera.h"
-#include "Experiment.h"
-#include "Funky.h"
 #include "MainMenuManager.h"
 #include "PlayerCamera.h"
 #include "PlayerController.h"
-#include "SomeScript.h"
 
 
 void Hachiko::Scripting::BackToMainMenu::DeserializeFrom(std::unordered_map<std::string, SerializedField>& serialized_fields)
@@ -85,70 +82,6 @@ void Hachiko::Scripting::DynamicCamera::SerializeTo(std::unordered_map<std::stri
 	serialized_fields["_speed"] = SerializedField(std::string("_speed"), std::make_any<float>(_speed), std::string("float"));
 
 	serialized_fields["_lerp_position"] = SerializedField(std::string("_lerp_position"), std::make_any<float>(_lerp_position), std::string("float"));
-}
-
-void Hachiko::Scripting::Experiment::DeserializeFrom(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::DeserializeFrom(serialized_fields);
-}
-
-void Hachiko::Scripting::Experiment::SerializeTo(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::SerializeTo(serialized_fields);
-}
-
-void Hachiko::Scripting::Funky::DeserializeFrom(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::DeserializeFrom(serialized_fields);
-
-	if(serialized_fields.find("_lerp_index") != serialized_fields.end())
-	{
-		const SerializedField& _lerp_index_sf = serialized_fields["_lerp_index"];
-		if (_lerp_index_sf.type_name == "int")
-		{
-			_lerp_index = std::any_cast<int>(_lerp_index_sf.copy);
-		}
-	}
-
-	if(serialized_fields.find("_lerp_position") != serialized_fields.end())
-	{
-		const SerializedField& _lerp_position_sf = serialized_fields["_lerp_position"];
-		if (_lerp_position_sf.type_name == "float")
-		{
-			_lerp_position = std::any_cast<float>(_lerp_position_sf.copy);
-		}
-	}
-
-	if(serialized_fields.find("_lerp_reverse") != serialized_fields.end())
-	{
-		const SerializedField& _lerp_reverse_sf = serialized_fields["_lerp_reverse"];
-		if (_lerp_reverse_sf.type_name == "bool")
-		{
-			_lerp_reverse = std::any_cast<bool>(_lerp_reverse_sf.copy);
-		}
-	}
-
-	if(serialized_fields.find("_initial_color") != serialized_fields.end())
-	{
-		const SerializedField& _initial_color_sf = serialized_fields["_initial_color"];
-		if (_initial_color_sf.type_name == "math::float4")
-		{
-			_initial_color = std::any_cast<math::float4>(_initial_color_sf.copy);
-		}
-	}
-}
-
-void Hachiko::Scripting::Funky::SerializeTo(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::SerializeTo(serialized_fields);
-
-	serialized_fields["_lerp_index"] = SerializedField(std::string("_lerp_index"), std::make_any<int>(_lerp_index), std::string("int"));
-
-	serialized_fields["_lerp_position"] = SerializedField(std::string("_lerp_position"), std::make_any<float>(_lerp_position), std::string("float"));
-
-	serialized_fields["_lerp_reverse"] = SerializedField(std::string("_lerp_reverse"), std::make_any<bool>(_lerp_reverse), std::string("bool"));
-
-	serialized_fields["_initial_color"] = SerializedField(std::string("_initial_color"), std::make_any<math::float4>(_initial_color), std::string("math::float4"));
 }
 
 void Hachiko::Scripting::MainMenuManager::DeserializeFrom(std::unordered_map<std::string, SerializedField>& serialized_fields)
@@ -443,47 +376,4 @@ void Hachiko::Scripting::PlayerController::SerializeTo(std::unordered_map<std::s
 	serialized_fields["_speed_y"] = SerializedField(std::string("_speed_y"), std::make_any<float>(_speed_y), std::string("float"));
 
 	serialized_fields["_starting_position"] = SerializedField(std::string("_starting_position"), std::make_any<math::float3>(_starting_position), std::string("math::float3"));
-}
-
-void Hachiko::Scripting::SomeScript::DeserializeFrom(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::DeserializeFrom(serialized_fields);
-
-	if(serialized_fields.find("_initial_position") != serialized_fields.end())
-	{
-		const SerializedField& _initial_position_sf = serialized_fields["_initial_position"];
-		if (_initial_position_sf.type_name == "math::float3")
-		{
-			_initial_position = std::any_cast<math::float3>(_initial_position_sf.copy);
-		}
-	}
-
-	if(serialized_fields.find("_position_offset") != serialized_fields.end())
-	{
-		const SerializedField& _position_offset_sf = serialized_fields["_position_offset"];
-		if (_position_offset_sf.type_name == "math::float3")
-		{
-			_position_offset = std::any_cast<math::float3>(_position_offset_sf.copy);
-		}
-	}
-
-	if(serialized_fields.find("_lerp_position") != serialized_fields.end())
-	{
-		const SerializedField& _lerp_position_sf = serialized_fields["_lerp_position"];
-		if (_lerp_position_sf.type_name == "float")
-		{
-			_lerp_position = std::any_cast<float>(_lerp_position_sf.copy);
-		}
-	}
-}
-
-void Hachiko::Scripting::SomeScript::SerializeTo(std::unordered_map<std::string, SerializedField>& serialized_fields)
-{
-	Hachiko::Scripting::Script::SerializeTo(serialized_fields);
-
-	serialized_fields["_initial_position"] = SerializedField(std::string("_initial_position"), std::make_any<math::float3>(_initial_position), std::string("math::float3"));
-
-	serialized_fields["_position_offset"] = SerializedField(std::string("_position_offset"), std::make_any<math::float3>(_position_offset), std::string("math::float3"));
-
-	serialized_fields["_lerp_position"] = SerializedField(std::string("_lerp_position"), std::make_any<float>(_lerp_position), std::string("float"));
 }
