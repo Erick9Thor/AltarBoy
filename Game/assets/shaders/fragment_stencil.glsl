98cde95c1f984cc62d2dec82c0f516ef01ca11d1
Baran Surucu <baransrc@gmail.com>
2022-04-17 03:49:47 +0200

    Added assets folder with correct casing and non-duplicate folders.

---
+++ Game/assets/shaders/fragment_stencil.glsl
@@ -0,0 +1,16 @@
+# version 440
+
+layout(std140, row_major, binding = 0) uniform Camera
+{
+    mat4 view;
+    mat4 proj;
+    vec3 pos;
+} camera;
+
+// Outputs
+out vec4 color;
+
+void main()
+{
+    color = vec4(1.0, 0.0, 0.0, 1.0);
+}
---
+++ Game/assets/shaders/vertex.glsl
@@ -0,0 +1,35 @@
+# version 440
+
+layout(location=0) in vec3 position;
+layout(location=1) in vec3 in_normal;
+layout(location=2) in vec2 in_tex_coord;
+layout(location=3) in vec3 in_tangent;
+
+layout(std140, row_major, binding = 0) uniform Camera
+{
+    mat4 view;
+    mat4 proj;
+    vec3 pos;
+} camera;
+
+uniform mat4 model;
+
+// Outputs
+struct VertexData
+{
+    vec3 normal;
+    vec3 tangent;
+    vec3 pos;
+    vec2 tex_coord;
+};
+out VertexData fragment;
+
+void main()
+{
+    gl_Position = camera.proj * camera.view *  model * vec4(position, 1.0);
+
+    fragment.normal = transpose(inverse(mat3(model))) * in_normal;
+    fragment.tangent = transpose(inverse(mat3(model))) * in_tangent;
+    fragment.pos = vec3(model * vec4(position, 1.0));
+    fragment.tex_coord = in_tex_coord;   
+}
