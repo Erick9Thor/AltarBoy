a005e6d684f524a318e8044d4fbd9b38fb5fb2bb
Alvaro Soppelsa <alvaro.soppelsa1@hp.com>
2022-04-27 21:40:42 +0200

    Add ResourceNavMesh class

---
+++ Source/src/resources/ResourceNaveMesh.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "Resource.h"
+
+namespace Hachiko
+{
+    class ResourceNavMesh : public Resource
+    {
+    public:
+        ResourceNavMesh() = default;
+        ~ResourceNavMesh() = default;
+
+    private:
+
+    };
+}
