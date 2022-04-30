ad378e50f0e69d90cb8bb12e0d73356b318b312a
Eric <erictoper@gmail.com>
2022-04-30 18:24:34 +0200

    [ADD] Add boolean skin on shader

--- Game/assets/shaders/vertex.glsl
+++ Game/assets/shaders/vertex.glsl
@@ -1,13 +1,15 @@
 # version 440
 
-#define MAX_BONES 64
-
-layout(location=0) in vec3 position;
+layout(location=0) in vec3 in_position;
 layout(location=1) in vec3 in_normal;
 layout(location=2) in vec2 in_tex_coord;
 layout(location=3) in vec3 in_tangent;
-layout(location=4) in ivec4 bone_indices;
-layout(location=5) in vec4 bone_weights;
+layout(location=4) in ivec4 in_bone_indices;
+layout(location=5) in vec4 in_bone_weights;
+
+#define MAX_BONES 64
+
+uniform bool hasBones;
 
 layout(std140, row_major, binding = 0) uniform Camera
 {
@@ -15,6 +17,13 @@ layout(std140, row_major, binding = 0) uniform Camera
     mat4 proj;
     vec3 pos;
 } camera;
+
+uniform mat4 model;
+
+layout(std140, row_major) uniform Skining
+{
+    mat4 palette[MAX_BONES];
+};
 
 // Outputs
 struct VertexData
@@ -26,36 +35,24 @@ struct VertexData
 };
 out VertexData fragment;
 
-uniform mat4 model;
-
-vec3 aux_pos;
-vec3 aux_normal;
-
-// SKINING
-
-layout(std140, row_major) uniform Skining
-{
-    mat4 palette[MAX_BONES];
-};
-
-void TransformOutput()
-{
-    mat4 skin_transform = palette[bone_indices[0]]*bone_weights[0]+palette[bone_indices[1]]*bone_weights[1]+
-                          palette[bone_indices[2]]*bone_weights[2]+palette[bone_indices[3]]*bone_weights[3];
-
-    aux_pos = (skin_transform*vec4(position, 1.0)).xyz;
-    aux_normal = (skin_transform*vec4(in_normal, 0.0)).xyz;
-}
-
 void main()
 {
+    vec4 position = vec4(in_position, 1.0);
+    vec4 normal = vec4(in_normal, 0.0);
 
-    TransformOutput();
+    if (hasBones)
+    { 
+        mat4 skin_transform = palette[in_bone_indices[0]] * in_bone_weights[0] + palette[in_bone_indices[1]] * in_bone_weights[1] +
+            palette[in_bone_indices[2]] * in_bone_weights[2] + palette[in_bone_indices[3]] * in_bone_weights[3];
 
-    gl_Position = camera.proj * camera.view *  model * vec4(aux_pos, 1.0);
+        position = (skin_transform*vec4(in_position, 1.0));
+        normal = (skin_transform*vec4(in_normal, 0.0));
+    }
 
-    fragment.normal = transpose(inverse(mat3(model))) * aux_normal;
+    gl_Position = camera.proj * camera.view *  model * position;
+
+    fragment.pos = vec3(model * position);
+    fragment.normal = normalize(transpose(inverse(mat3(model))) * in_normal.xyz);
     fragment.tangent = transpose(inverse(mat3(model))) * in_tangent;
-    fragment.pos = vec3(model * vec4(aux_pos, 1.0));
-    fragment.tex_coord = in_tex_coord;   
+    fragment.tex_coord = in_tex_coord;
 }
