ee0039cc22cbcba3208f3d52d6418147e738e95f
Baran Surucu <baransrc@gmail.com>
2022-05-21 04:38:56 +0200

    Implemented lighting pass vertex shader. (it's so advanced)

--- Game/assets/shaders/vertex_deferred_lighting.glsl
+++ Game/assets/shaders/vertex_deferred_lighting.glsl
@@ -1,10 +1,12 @@
 # version 460
 
-layout(location=0) in vec2 input_texture_coords;
+layout(location = 0) in vec3 input_position;
+layout(location = 1) in vec2 input_texture_coords;
 
 out vec2 texture_coords;
 
 void main()
 {
+    gl_Position = vec4(input_position, 1.0f);
     texture_coords = input_texture_coords;
 }
