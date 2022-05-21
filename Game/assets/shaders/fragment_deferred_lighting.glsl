1a10d9c95a1fa8cf127d4cb70cc40c9cbc4ba7fd
Baran Surucu <baransrc@gmail.com>
2022-05-21 04:39:45 +0200

    Implemented lighting pass fragment shader without lighting calculations. (it can render all g buffer data)

--- Game/assets/shaders/fragment_deferred_lighting.glsl
+++ Game/assets/shaders/fragment_deferred_lighting.glsl
@@ -4,40 +4,40 @@ out vec4 fragment_color;
 
 in vec2 texture_coords;
 
-uniform float mode;
-uniform sampler2D g_diffuse;
-uniform sampler2D g_specular_smoothness;
-uniform sampler2D g_normal;
-uniform sampler2D g_position;
+uniform int mode;
+layout (binding = 0) uniform sampler2D g_specular_smoothness;
+layout (binding = 1) uniform sampler2D g_diffuse;
+layout (binding = 2) uniform sampler2D g_normal;
+layout (binding = 3) uniform sampler2D g_position;
 
 // TODO: Add lights and stuff.
 
 void main()
 {
-    vec3  fragment_diffuse = texture(g_diffuse, texture_coords).rgb;
-    vec3  fragment_specular = texture(g_specular_smoothness, texture_coords).rgb;
-    float fragment_smoothness = texture(g_specular_smoothness, texture_coords).a;
-    vec3  fragment_normal = normalize(texture(g_normal, texture_coords).rgb);
-    vec3  fragment_position = texture(g_position, texture_coords).rgb;
-    
-    if (mode == 0.0f)
+    vec3  fragment_diffuse = (texture(g_diffuse, texture_coords)).rgb;
+    vec3  fragment_specular = (texture(g_specular_smoothness, texture_coords)).rgb;
+    float fragment_smoothness = (texture(g_specular_smoothness, texture_coords)).a;
+    vec3  fragment_normal = (texture(g_normal, texture_coords)).rgb;
+    vec3  fragment_position = (texture(g_position, texture_coords)).rgb;
+
+    if (mode == 0)
     {
-        fragment_color.rgb = fragment_diffuse;
+        fragment_color = vec4(fragment_diffuse, 1.0f);
     }
-    else if (mode == 1.0f)
+    else if (mode == 1)
     {
-        fragment_color.rgb = fragment_specular;
+        fragment_color = vec4(fragment_specular, 1.0f);
     }
-    else if (mode == 2.0f)
+    else if (mode == 2)
     {
-        fragment_color.rgb = vec3(fragment_smoothness, fragment_smoothness, fragment_smoothness);
+        fragment_color = vec4(fragment_smoothness, fragment_smoothness, fragment_smoothness, 1.0f);
     }
-    else if (mode == 3.0f)
+    else if (mode == 3)
     {
-        fragment_color.rgb = fragment_normal;
+        fragment_color = vec4(fragment_normal, 1.0f);
     }
     else
     {
-        fragment_color.rgb = fragment_position;
+        fragment_color = vec4(fragment_position, 1.0f);
     }
 }
