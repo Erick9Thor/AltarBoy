ddc2fa11492137cb4f6f5a2862041a5e4ab8e410
Baran Surucu <baransrc@gmail.com>
2022-05-22 03:28:45 +0200

    Added correct normal calculation of deferred rendering geometry pass.

--- Game/assets/shaders/fragment_deferred_geometry.glsl
+++ Game/assets/shaders/fragment_deferred_geometry.glsl
@@ -41,6 +41,29 @@ struct VertexData
 in VertexData fragment;
 
 uniform sampler2D textures[N_2D_SAMPLERS];
+
+mat3 CreateTangentSpace(const vec3 normal, const vec3 tangent)
+{
+	vec3 ortho_tangent = normalize(tangent - dot(tangent, normal) * normal); // Gram-Schmidt
+	vec3 bitangent = cross(normal, ortho_tangent);
+	return mat3(tangent, bitangent, normal);
+}
+
+// TODO: Be sure that this is correct.
+vec3 CalculateNormal(uint normal_flag, vec3 texture_normal_coords, vec3 fragment_normal_coords, vec3 fragment_tangent)
+{
+    vec3 normal = normalize(fragment_normal_coords);
+
+    if (normal_flag > 0)
+    {
+        mat3 tbn = CreateTangentSpace(normal, normalize(fragment_tangent));
+        vec3 texture_normal = normalize(texture_normal_coords.xyz);
+	    vec3 fragment_normal = tbn * (texture_normal * 2.0 - 1.0);
+	    normal = normalize(fragment_normal);
+    }
+
+    return normal;
+}
  
 void main()
 {
@@ -48,7 +71,7 @@ void main()
 
     // Store diffuse color in rgb channels of g buffer texture for diffuse: 
     g_diffuse.rgb = material.diffuse_flag > 0 
-         ? texture(textures[DIFFUSE_SAMPLER], fragment.tex_coord).rgb 
+         ? (pow(texture(textures[DIFFUSE_SAMPLER], fragment.tex_coord), vec4(2.2))).rgb 
          : material.diffuse_color.rgb;
     g_diffuse.a = 1.0f;
 
@@ -60,7 +83,11 @@ void main()
     g_specular_smoothness.a = material.smoothness;
 
     // Store the fragment normal in rgb channels of g buffer texture for normal:
-    g_normal.xyz = normalize(fragment.normal).xyz;
+    g_normal.xyz = CalculateNormal(
+        material.normal_flag, 
+        texture(textures[NORMAL_SAMPLER], fragment.tex_coord).rgb, 
+        fragment.normal, 
+        fragment.tangent); 
 
     // Store the fragment position in rgb channels of g buffer texture for position:
     g_position.xyz = fragment.pos.xyz;
