0e3e0377ac5c8f5b15c525c8fcff384e9c92c14b
Armando <me@armando.dev>
2022-05-30 01:35:09 +0200

    fixing duplicated handling of mouse click on user interface

--- Source/src/events/MouseEventPayload.h
+++ Source/src/events/MouseEventPayload.h
@@ -12,20 +12,24 @@ namespace Hachiko
             RELEASE,
         };
 
-        MouseEventPayload(Action action, float2 coords) : action(action), coords(coords) {}
+        MouseEventPayload(Action action, const float2& coords) :
+            action(action),
+            coords(coords)
+        {
+        }
 
         [[nodiscard]] Action GetAction() const
         {
             return action;
         }
 
-        [[nodiscard]] float2 GetCoords() const
+        [[nodiscard]] const float2& GetCoords() const
         {
             return coords;
         }
 
     private:
-        float2 coords;
         Action action;
+        float2 coords;
     };
 }
