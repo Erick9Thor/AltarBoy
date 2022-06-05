3637f200297ce5c04e94811dde4824f064c1df7b
Bernat <bolicubo@gmail.com>
2022-06-06 01:12:18 +0200

    Added missing script

--- Gameplay/RoomTeleporter.cpp
+++ Gameplay/RoomTeleporter.cpp
@@ -8,30 +8,78 @@ Hachiko::Scripting::RoomTeleporter::RoomTeleporter(GameObject* game_object)
 	, _touching(false)
 	, _target(nullptr)
 	, _fade_image(nullptr)
-
+	, _fade_duration(0.25f)
 {
 }
 
 void Hachiko::Scripting::RoomTeleporter::OnAwake()
 {
-	// Don't forget, if you change any field here, it will override whatever 
-	// you set from the editor, as OnLoad is called before OnAwake. As opposed
-	// to OnStart, OnAwake is called again when you build scripts again at run
-	// time.
+
 }
 
 void Hachiko::Scripting::RoomTeleporter::OnStart()
 {
+	// Don't forget, if you change any field here, it will override whatever 
+	// you set from the editor, as OnLoad is called before OnAwake. As opposed
+	// to OnStart, OnAwake is called again when you build scripts again at run
+	// time.
+
+	if (!_fade_image) return;
+
+	_image_transform = _fade_image->GetGameObject()->GetComponent<ComponentTransform2D>();
+
+	if (!_image_transform) return;
+
+	// Center image on the screen
+	_image_transform->SetAnchor(float2(0.5));
+
+	// Make image occupy entire screen (Scale multiplies final size)
+	_image_transform->SetSize(float2(10.f));
+	_image_transform->SetScale(float2(25000.f));
+
+	// Set initial color to black without alpha
+	_fade_image->SetColor(_clear_color);
 }
 
 void Hachiko::Scripting::RoomTeleporter::OnUpdate()
 {
-	if (_target->GetTransform()->GetGlobalPosition().Distance(game_object->GetTransform()->GetGlobalPosition()) <= 2.0f)
+	
+	//if (_target->GetTransform()->GetGlobalPosition().Distance(game_object->GetTransform()->GetGlobalPosition()) <= 2.0f)
+	if(Input::IsKeyDown(Input::KeyCode::KEY_T))
 	{
-		// Check the ComponentImage to see the changes I've made to expose it
-		// to Gameplay module.
-		// I also added Set and Get methods for you, have a quality development
-		// time, hmu for any further questions.
-		_fade_image->SetColor(float4(1.0f, 0.0f, 0.0f, 0.5f));
+		SetActive(!_transition_active);
+	}
+
+	if (_changing)
+	{
+		Interpolate();
+	}
+}
+
+void Hachiko::Scripting::RoomTeleporter::SetActive(bool v)
+{
+	_transition_active = v;
+	_fade_progress = 0.f;
+	_changing = true;
+}
+
+void Hachiko::Scripting::RoomTeleporter::Interpolate()
+{
+	_fade_progress += Time::DeltaTime() / _fade_duration;
+	//_fade_progress = _fade_progress > 1.0f ? 1.0f : _fade_progress;
+
+	if (_transition_active)
+	{
+		_fade_image->SetColor(float4::Lerp(_clear_color, _opaque_color, _fade_progress));
+	}
+	else
+	{
+		_fade_image->SetColor(float4::Lerp(_opaque_color, _clear_color, _fade_progress));
+	}
+		
+
+	if (_fade_progress >= 1.f)
+	{
+		_changing = false;
 	}
 }
--- Gameplay/RoomTeleporter.h
+++ Gameplay/RoomTeleporter.h
@@ -21,13 +21,22 @@ namespace Hachiko
             void OnStart() override;
             void OnUpdate() override;
 
+            void SetActive(bool v);
+
         private:
+            void Interpolate();
 
         public:
+            const float4 _clear_color = float4::zero;
+            const float4 _opaque_color = float4(0.f, 0.f, 0.f, 1.f);
+            float _fade_progress = 0.f;
+            bool _transition_active = false;
+            bool _changing = false;
             SERIALIZE_FIELD(bool, _touching);
             SERIALIZE_FIELD(GameObject*, _target);
             SERIALIZE_FIELD(ComponentImage*, _fade_image);
-
+            SERIALIZE_FIELD(float, _fade_duration);
+            ComponentTransform2D* _image_transform = nullptr;
         private:
         };
 } // namespace Scripting
