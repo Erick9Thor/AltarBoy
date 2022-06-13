d715910f26f986a632f0f715f5553cfcad0b8a86
miquelmiro3 <miquel.miro3@gmail.com>
2022-06-13 21:10:55 +0200

    Shader changes

--- Game/shaders/fragment_deferred_lighting.glsl
+++ Game/shaders/fragment_deferred_lighting.glsl
@@ -20,12 +20,14 @@ layout (binding = 4) uniform sampler2D g_emissive;
 
 void main()
 {
+
     vec3  fragment_diffuse = (texture(g_diffuse, texture_coords)).rgb;
-    vec3  fragment_specular = (texture(g_specular_smoothness, texture_coords)).rgb;
-    float fragment_smoothness = (texture(g_specular_smoothness, texture_coords)).a;
+    vec4 fragment_specular_smoothness = texture(g_specular_smoothness, texture_coords);
     vec3  fragment_normal = (texture(g_normal, texture_coords)).rgb;
     vec3  fragment_position = (texture(g_position, texture_coords)).rgb;
     vec3  fragment_emissive = (texture(g_emissive, texture_coords)).rgb;
+    vec3  fragment_specular = fragment_specular_smoothness.rgb;
+    float fragment_smoothness = fragment_specular_smoothness.a;
     vec3  view_direction = normalize(camera.pos - fragment_position);
     
     if (mode == 0)
