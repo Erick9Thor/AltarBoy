d2afb41c5a9ca43a3edaee8f980cf3e6607dfeec
Monica <monicamartin13@hotmail.com>
2022-06-22 15:02:24 +0200

    Typo fix

--- Source/src/Gameplay.h
+++ Source/src/Gameplay.h
@@ -384,7 +384,7 @@ enum class HACHIKO_API KeyCode
 enum class HACHIKO_API MouseButton
 {
     // These are taken from SDL source code.
-    UNKOWN = 0,
+    UNKNOWN = 0,
     LEFT = 1,
     MIDDLE = 2,
     RIGHT = 3
