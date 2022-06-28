61d8f1ebb60486c8c9024dd815925e25b56c54f7
Baran Surucu <baransrc@gmail.com>
2022-06-28 17:33:28 +0200

    Moved shadow related functions into a common file.

--- Game/shaders/fragment_deferred_lighting.glsl
+++ Game/shaders/fragment_deferred_lighting.glsl
@@ -6,6 +6,7 @@
 #include "/common/uniforms/camera_uniform.glsl"
 #include "/common/uniforms/lights_uniform.glsl"
 #include "/common/functionality/lighting_functions.glsl"
+#include "/common/functionality/shadow_functions.glsl"
 
 out vec4 fragment_color;
 in vec2 texture_coords;
@@ -25,118 +26,6 @@ layout (binding = 3) uniform sampler2D g_position;
 layout (binding = 4) uniform sampler2D g_emissive;
 layout (binding = 5) uniform sampler2D shadow_map;
 
-float CalculateShadow(vec4 fragment_position_from_light, sampler2D shadow_map_texture, float bias)
-{
-    // Perform perspective division:
-    // NOTE: On the tutorials it is said that since we are using an orthographic projection,
-    // this division is actually meaningless as w is always 1.0. However this step is necessary
-    // when using perspective projection so we do it anyways to support perspective projection
-    // out of the box.
-    vec3 projection_coordinates = fragment_position_from_light.xyz / fragment_position_from_light.w;
-
-    // The projection_coordinates will be returned in the range [-1, 1]. Because the depth from 
-    // the shadow map is in the range [0,1] and we also want to use projection_coordinates from 
-    // the shadow map, we transform the NDC coordinates to the range [0,1]:
-    projection_coordinates = projection_coordinates * 0.5 + 0.5;
-
-    // With these projected coordinats we can sample the depth map as the resulting [0,1] 
-    // coordinates from projection_coordinates directly correspond to the transformed NDC coords
-    // from the first shadow depth render pass. This calculation gives us the closest depth from
-    // the light's perspective:
-    float closest_depth = texture(shadow_map_texture, projection_coordinates.xy).r;
-
-    // We can get the current depth at this fragment by retrieving the projected vector's z coords
-    // which equals to the depth of this fragment from the light's perspective:
-    float current_depth = projection_coordinates.z;
-
-    // Now with closest_depth and current_depth in our hands, we check whether current_depth is 
-    // higher than closest_depth, and if so this fragment is in shadow:
-    float shadow = current_depth - bias > closest_depth ? 1.0f : 0.0f;
-
-    return shadow; 
-}
-
-float linstep(float low, float high, float value)
-{
-    return clamp((value - low) / (high - low), 0.0, 1.0);
-}
-
-float Chebyshev(float current_depth, vec2 moments, float min_variance_value, float light_bleeding_reduction_amount_value)
-{
-    if (current_depth <= moments.x)
-    {
-        return 1.0;
-    }
-
-    float variance = min(1.0f, max(moments.y - moments.x * moments.x, min_variance_value));
-
-    float distance_depth = current_depth - moments.x;
-
-    float p_max = variance / (variance + distance_depth * distance_depth);
-          p_max = linstep(light_bleeding_reduction_amount_value, 1.0, p_max);
-
-    return p_max;
-}
-
-float CalculateShadowVariance(sampler2D shadow_map_texture, vec4 fragment_position_from_light, float min_variance_value, float light_bleeding_reduction_amount_value)
-{
-    // Perform perspective division:
-    // NOTE: On the tutorials it is said that since we are using an orthographic projection,
-    // this division is actually meaningless as w is always 1.0. However this step is necessary
-    // when using perspective projection so we do it anyways to support perspective projection
-    // out of the box.
-    vec3 projection_coordinates = fragment_position_from_light.xyz / fragment_position_from_light.w;
-
-    // The projection_coordinates will be returned in the range [-1, 1]. Because the depth from 
-    // the shadow map is in the range [0,1] and we also want to use projection_coordinates from 
-    // the shadow map, we transform the NDC coordinates to the range [0,1]:
-    projection_coordinates = projection_coordinates * 0.5 + vec3(0.5);
-
-    // We can get the current depth at this fragment by retrieving the projected vector's z coords
-    // which equals to the depth of this fragment from the light's perspective:
-    float current_depth = projection_coordinates.z;
-
-    vec2 moments = texture(shadow_map_texture, projection_coordinates.xy).xy;
-
-    float shadow = Chebyshev(current_depth, moments, min_variance_value, light_bleeding_reduction_amount_value);
-
-    return shadow;
-}
-
-float CalculateShadowExponentialVariance(sampler2D shadow_map_texture, vec4 fragment_position_from_light, float min_variance_value, float light_bleeding_reduction_amount_value, float shadow_bias)
-{
-    // Perform perspective division:
-    // NOTE: On the tutorials it is said that since we are using an orthographic projection,
-    // this division is actually meaningless as w is always 1.0. However this step is necessary
-    // when using perspective projection so we do it anyways to support perspective projection
-    // out of the box.
-    vec3 projection_coordinates = fragment_position_from_light.xyz / fragment_position_from_light.w;
-
-    // Apply z bias:
-    projection_coordinates.z -= shadow_bias;
-
-    // The projection_coordinates will be returned in the range [-1, 1]. Because the depth from 
-    // the shadow map is in the range [0,1] and we also want to use projection_coordinates from 
-    // the shadow map, we transform the NDC coordinates to the range [0,1]:
-    projection_coordinates = (projection_coordinates * 0.5) + vec3(0.5);
-
-    float shadow = 0.0f;
-
-    vec4 moments = texture(shadow_map_texture, projection_coordinates.xy);
-
-    projection_coordinates = (projection_coordinates * 2.0) - vec3(1.0);
-
-    float positive = exp(exponent * projection_coordinates.z);
-    float negative = -exp(-exponent * projection_coordinates.z);
-
-    float positive_shadow = Chebyshev(positive, moments.xy, min_variance_value, light_bleeding_reduction_amount_value);
-    float negative_shadow = Chebyshev(negative, moments.zw, min_variance_value, light_bleeding_reduction_amount_value);
-    
-    shadow = min(positive_shadow, negative_shadow);
-	
-    return shadow;
-}
-
 void main()
 {
 
@@ -153,9 +42,14 @@ void main()
     if (mode == 0)
     {
         vec3 hdr_color = vec3(0.0);
-        float fragment_shadow = CalculateShadowExponentialVariance(shadow_map, fragment_position_from_light, min_variance, light_bleeding_reduction_amount, shadow_bias);
-        // float fragment_shadow = 0.0;
-
+        float fragment_shadow = CalculateShadowExponentialVariance(
+            shadow_map, 
+            fragment_position_from_light, 
+            min_variance, 
+            light_bleeding_reduction_amount, 
+            shadow_bias, 
+            exponent);
+            
         hdr_color += vec3(fragment_shadow) * DirectionalPBR(fragment_normal, view_direction, lights.directional, fragment_diffuse, fragment_specular, fragment_smoothness);
         
         for(uint i=0; i<lights.n_points; ++i)
