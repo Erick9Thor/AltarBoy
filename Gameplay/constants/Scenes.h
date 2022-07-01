94d1ce5a8c2fdddb39f050ff333d22c353a19591
Bernat <bolicubo@gmail.com>
2022-07-01 22:58:34 +0200

    Forgot a file

--- Gameplay/constants/Scenes.h
+++ Gameplay/constants/Scenes.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "Gameplay.h"
+
 namespace Hachiko::Scenes
 {
 	static UID GAME = 3947210229329588811;
@@ -8,4 +10,26 @@ namespace Hachiko::Scenes
 	static UID MAIN_MENU = 3083261127926411514;
 	static const char* player_go_name = "Player";
 	static const char* enemies_go_name = "Enemies";
+	static const char* level_manager_go_name = "LevelManager";
+	static const char* terrain_container_go_name = "Level";
+
+	static GameObject* GetPlayer()
+	{
+		return SceneManagement::FindInCurrentScene(player_go_name);
+	}
+
+	static GameObject* GetEnemiesContainer()
+	{
+		return SceneManagement::FindInCurrentScene(enemies_go_name);
+	}
+
+	static GameObject* GetLevelManager()
+	{
+		return SceneManagement::FindInCurrentScene(level_manager_go_name);
+	}
+
+	static GameObject* GetTerrainContainer()
+	{
+		return SceneManagement::FindInCurrentScene(terrain_container_go_name);
+	}
 }
