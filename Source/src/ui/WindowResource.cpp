246a79724ead634d8c454e2e7d933b32a9973dbb
Alvaro Soppelsa <alvarosoppelsa@gmail.com>
2022-07-29 20:21:29 +0200

    Refactor Window Resources

--- Source/src/ui/WindowResource.cpp
+++ Source/src/ui/WindowResource.cpp
@@ -12,8 +12,15 @@ Hachiko::WindowResource::WindowResource() :
 {
 }
 
+void Hachiko::WindowResource::Init()
+{
+    root_directory = "./assets";
+    current_path = root_directory;
+}
+
 void Hachiko::WindowResource::Update()
 {
+
     ImGui::SetNextWindowDockID(App->editor->dock_down_id, ImGuiCond_FirstUseEver);
     if (!ImGui::Begin(StringUtils::Concat(ICON_FA_IMAGES, " ", name).c_str(), &active, ImGuiWindowFlags_NoNavInputs))
     {
@@ -21,8 +28,100 @@ void Hachiko::WindowResource::Update()
         return;
     }
 
-    // WIP: create material 
-    if (ImGui::Button("Create material (WIP)"))
+
+
+    if (!updated)
+    {
+        UpdateDirectoryInfo();
+        ImGui::End();
+        return;
+    }
+
+    static float padding = 16.0f;
+    static float thumbnail_size = 128.0f; 
+    static float cell_size = thumbnail_size + padding;
+    float panel_width = ImGui::GetContentRegionAvail().x;
+    int column_count = (int)(panel_width / cell_size);
+    if (column_count < 1)
+    {
+        column_count = 1;
+    }
+
+    ImGui::Columns(column_count, 0, false);
+
+    if (!current_path.string()._Equal(root_directory.string()))
+    {
+        if (ImGui::Button(ICON_FA_FOLDER_MINUS, {thumbnail_size, thumbnail_size * 0.3f}))
+        {
+            current_path = current_path.parent_path();
+            updated = false;
+            ImGui::End();
+            return;
+        }
+        ImGui::NextColumn();
+    }
+
+    CreateMaterialGui({thumbnail_size, thumbnail_size * 0.3f});
+
+    for (const auto& directory_entry : current_content)
+    {
+        if (directory_entry.is_directory)
+        {
+            if (ImGui::Button(StringUtils::Concat(ICON_FA_FOLDER, " ", directory_entry.name).c_str(), {thumbnail_size, thumbnail_size * 0.3f}))
+            {
+                current_path = directory_entry.path;
+                updated = false;
+                break;
+            }
+            ImGui::NextColumn();
+        }
+    }
+
+    ImGui::Columns(1);
+    ImGui::Separator();
+
+    for (const auto& directory_entry : current_content)
+    {
+        if (!directory_entry.is_directory)
+        {
+            if (FileSystem::GetFileExtension(directory_entry.name.c_str()) == META_EXTENSION)
+            {
+                continue;
+            }
+
+            auto selection = ImGui::Selectable(directory_entry.name.c_str(), true, ImGuiSelectableFlags_AllowDoubleClick);
+            
+            if (ImGui::IsItemHovered())
+            {
+                if (ImGui::IsMouseDoubleClicked(selection))
+                {
+                    LoadAsset(directory_entry.path.string());
+                    App->event->Publish(Event::Type::CREATE_EDITOR_HISTORY_ENTRY);
+                }
+            }
+        }
+    }
+
+    ImGui::End();
+}
+
+void Hachiko::WindowResource::LoadAsset(const std::string& path)
+{
+    HE_LOG("Resource file: %s", path.c_str());
+    if (!FileSystem::GetFileExtension(path.c_str())._Equal(META_EXTENSION))
+    {
+        App->resources->LoadAsset(path);
+    }
+}
+
+void Hachiko::WindowResource::CreateMaterialGui(const ImVec2 &button_size)
+{
+    if (current_path != "./assets\\materials")
+    {
+        return;
+    }
+
+    if (ImGui::Button("Create material", button_size))
     {
         ImGui::OpenPopup("CreateMaterialPopup");
         auxiliary_name = "NewMaterial";
@@ -39,49 +138,31 @@ void Hachiko::WindowResource::Update()
         ImGui::EndPopup();
     }
 
-    std::filesystem::path assets_path("./assets");
+    ImGui::NextColumn();
+}
 
-    if (current_directory != assets_path && ImGui::Button(ICON_FA_FOLDER_MINUS))
-    {
-        current_directory = current_directory.parent_path();
-    }    
+void Hachiko::WindowResource::UpdateDirectoryInfo()
+{
+    current_content.clear();
 
-    for (auto& directory_entry : std::filesystem::directory_iterator(current_directory))
+    for (auto& directory_entry : std::filesystem::directory_iterator(current_path))
     {
-        const auto& path = directory_entry.path();
-        auto relative_path = relative(path, assets_path);
-        std::string filename = relative_path.filename().string();
+        Entry tmp_entry;
+        tmp_entry.path = directory_entry.path();
 
         if (directory_entry.is_directory())
         {
-            if (ImGui::Button(filename.c_str()))
-            {
-                current_directory /= path.filename();
-            }
+            tmp_entry.is_directory = true;
+            tmp_entry.name = std::filesystem::relative(directory_entry.path(), current_path).string();
         }
         else
         {
-            if (FileSystem::GetFileExtension(filename.c_str()) != META_EXTENSION)
-            {
-                auto selection = ImGui::Selectable(filename.c_str(), ImGuiSelectableFlags_AllowDoubleClick);
-                if (ImGui::IsMouseDoubleClicked(selection) && ImGui::IsItemHovered())
-                {
-                    filename.insert(0, "\\");
-                    filename.insert(0, current_directory.string().c_str());
-                    LoadAsset(filename);
-                    App->event->Publish(Event::Type::CREATE_EDITOR_HISTORY_ENTRY);
-                }
-            }            
+            tmp_entry.is_directory = false;
+            tmp_entry.name = directory_entry.path().filename().string();
         }
-    }
-    ImGui::End();
-}
 
-void Hachiko::WindowResource::LoadAsset(const std::string& path)
-{
-    HE_LOG("Resource file: %s", path.c_str());
-    if (!FileSystem::GetFileExtension(path.c_str())._Equal(META_EXTENSION))
-    {
-        App->resources->LoadAsset(path);
+        current_content.emplace_back(tmp_entry);
     }
+
+    updated = true;
 }
--- Source/src/ui/WindowResource.h
+++ Source/src/ui/WindowResource.h
@@ -4,17 +4,30 @@
 
 namespace Hachiko
 {
+    struct Entry
+    {
+        bool is_directory = false;
+        std::filesystem::path path;
+        std::string name;
+    };
+
     class WindowResource final : public Window
     {
+
     public:
         WindowResource();
         ~WindowResource() override = default;
-
+        void Init() override;
         void Update() override;
     private:
         void LoadAsset(const std::string& path);
-        std::filesystem::path current_directory = "./assets";
-        std::string auxiliary_name;
+        void CreateMaterialGui(const ImVec2& button_size);
+        void UpdateDirectoryInfo();
 
+        bool updated = false;
+        std::string auxiliary_name;
+        std::filesystem::path current_path;
+        std::vector<Entry> current_content;
+        std::filesystem::path root_directory;
     };
 }
