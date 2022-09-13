4c3ebe69241da4b926659c47aac25040416df655
miquelmiro3 <miquel.miro3@gmail.com>
2022-09-13 19:08:15 +0200

    Ending level2 jumps to boss level

--- Gameplay/constants/Scenes.h
+++ Gameplay/constants/Scenes.h
@@ -9,6 +9,7 @@ namespace Hachiko::Scenes
 	static UID LOSE = 3083261127926411514;
 	static UID MAIN_MENU = 3083261127926411514;
 	static UID LEVEL2 = 8516499815050636676;
+	static UID BOSS_LEVEL = 9597392922211235555;
 	static const char* player_go_name = "Player";
 	static const char* boss_go_name = "Boss";
 	static const char* main_camera_go_name = "Main Camera";
