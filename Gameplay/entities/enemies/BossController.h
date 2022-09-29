ecc1326c173a0f09990b5384f7f30a6a8adc4a09
Baran Surucu <baransrc@gmail.com>
2022-09-30 00:10:01 +0200

    Cleaned up boss controller.

--- Gameplay/entities/enemies/BossController.h
+++ Gameplay/entities/enemies/BossController.h
@@ -11,40 +11,39 @@
 
 namespace Hachiko
 {
-	class GameObject;
-	class ComponentTransform;
-	class ComponentParticleSystem;
-	class ComponentBillboard;
-	class ComponentProgressBar;
+    class GameObject;
+    class ComponentTransform;
+    class ComponentParticleSystem;
+    class ComponentBillboard;
+    class ComponentProgressBar;
 
-	namespace Scripting
-	{
+    namespace Scripting
+    {
         class StalagmiteManager;
         class AudioManager;
-		class PlayerController;
+        class PlayerController;
         class EnemyController;
         class CombatVisualEffectsPool;
-		class PlayerCamera;
+        class PlayerCamera;
 
-		enum class BossState
-		{
-			WAITING_ENCOUNTER,
-			STARTING_ENCOUNTER,
-			COMBAT_FORM,
-			CACOON_FORM,
-			DEAD,
-		};
+        enum class BossState
+        {
+            WAITING_ENCOUNTER,
+            STARTING_ENCOUNTER,
+            COMBAT_FORM,
+            COCOON_FORM,
+            DEAD,
+        };
 
-		// States while in BossState::COMBAT_FORM
-		enum class CombatState
-		{
-			IDLE,
-			CHASING,
-			ATTACKING,
-			SPAWNING_CRYSTALS,
-			JUMPING,
-			//CRYSTAL_JUMP,
-		};
+        // States while in BossState::COMBAT_FORM
+        enum class CombatState
+        {
+            IDLE,
+            CHASING,
+            ATTACKING,
+            SPAWNING_CRYSTALS,
+            JUMPING,
+        };
 
         // States while the boss is in CombatState::JUMPING
         enum class JumpingState
@@ -83,124 +82,120 @@ namespace Hachiko
         {
             SERIALIZATION_METHODS(false)
 
-			enum class BossWeaponUsed
-			{
-				RED = 0,
-				GREEN,
-				BLUE,
-				SIZE
-			};
+            enum class BossWeaponUsed
+            {
+                RED = 0,
+                GREEN,
+                BLUE,
+                SIZE
+            };
 
-			struct BossAttack
-			{
-				float hit_delay = 0.f;
-				float duration = 0.f;
-				float cooldown = 0.2f;
-				CombatManager::AttackStats stats;
-			};
+            struct BossAttack
+            {
+                float hit_delay = 0.f;
+                float duration = 0.f;
+                float cooldown = 0.2f;
+                CombatManager::AttackStats stats;
+            };
 
-			enum class AttackType
-			{
-				MELEE_CLAW = 0,
-				SWORD,
-				HAMMER,
-			};
+            enum class AttackType
+            {
+                MELEE_CLAW = 0,
+                SWORD,
+                HAMMER,
+            };
 
-			struct Weapon
-			{
-				std::string name = "Undefined Weapon";
-				// Here we define the combo of attacks		
-				float4 color = float4::zero;
-				bool unlimited = true;
-				unsigned charges = 0;
-				GameObject* weapon_go = nullptr;
-				std::vector<BossAttack> attacks;
-			};
+            struct Weapon
+            {
+                std::string name = "Undefined Weapon";
+                // Here we define the combo of attacks		
+                float4 color = float4::zero;
+                bool unlimited = true;
+                unsigned charges = 0;
+                GameObject* weapon_go = nullptr;
+                std::vector<BossAttack> attacks;
+            };
 
-		public:
-			BossController(GameObject* game_object);
-			~BossController() override = default;
+        public:
+            BossController(GameObject* game_object);
+            ~BossController() override = default;
 
-			void OnAwake() override;
-			void OnStart() override;
-			void OnUpdate() override;
+            void OnAwake() override;
+            void OnStart() override;
+            void OnUpdate() override;
 
-			BossState GetState() const { return state; };
-			bool IsAlive() const;
-			void RegisterHit(int dmg);
-			void UpdateHpBar();
+            BossState GetState() const { return state; };
+            bool IsAlive() const;
+            void RegisterHit(int dmg);
+            void UpdateHpBar() const;
 
 
-		private:
-			
-			// WEAPON MANAGE
-			void ChangeWeapon(unsigned weapon_idx);
-			void CreateBossWeapons();
-			BossAttack GetAttackType(AttackType attack_type);
+        private:
+            // WEAPON MANAGE
+            void ChangeWeapon(unsigned weapon_idx);
+            void CreateBossWeapons();
+            static BossAttack GetAttackType(AttackType attack_type);
 
-			Weapon& GetCurrentWeapon();
-			const BossAttack& GetNextAttack();
-			const BossAttack& GetCurrentAttack();
+            Weapon& GetCurrentWeapon();
+            const BossAttack& GetNextAttack();
+            const BossAttack& GetCurrentAttack();
 
-			void SetUpHpBar();
-			void SetHpBarActive(bool v);
-			void StateController();
-			void StateTransitionController();
-			void CombatController();
-			void CombatTransitionController();
+            void SetUpHpBar();
+            void SetHpBarActive(bool v) const;
+            void StateController();
+            void StateTransitionController();
+            void CombatController();
+            void CombatTransitionController();
 
-			float GetPlayerDistance();
+            float GetPlayerDistance();
 
-			// Fight States
-			void WaitingController();
-			void StartEncounter();
-			void StartEncounterController();
-			void ResetCombatState();
-			void Die();
-			void DieController();
+            // Fight States
+            void WaitingController();
+            void StartEncounter();
+            void StartEncounterController();
+            void ResetCombatState();
+            void Die();
+            void DieController();
 
-			float4x4 GetMeleeAttackOrigin(float attack_range) const;
+            float4x4 GetMeleeAttackOrigin(float attack_range) const;
 
-			// Cacoon
-			void StartCacoon();
-			void CacoonController();
+            // Cacoon
+            void StartCocoon();
+            void CocoonController();
 
-			bool CacoonTrigger();
-			void FinishCacoon();
+            bool CocoonTrigger();
+            void FinishCocoon();
 
-			// Combat States (While COMBAT_FORM)
-			void Chase();
-			void ChaseController();
+            // Combat States (While COMBAT_FORM)
+            void Chase();
+            void ChaseController();
 
-			void MeleeAttack();
-			void MeleeAttackController();
+            void MeleeAttack();
+            void MeleeAttackController();
 
-			void SpawnCrystals();
-			void SpawnCrystalsController();
+            void SpawnCrystals();
+            void SpawnCrystalsController();
 
-			void FocusCamera(bool focus_on_boss);
+            void FocusCamera(bool focus_on_boss);
 
-			void SpawnEnemy();
-			void ResetEnemies();
+            void SpawnEnemy();
+            void ResetEnemies();
 
+            // Jumping related methods:
             void TriggerJumpingState(JumpingMode jumping_mode);
             void ResetJumpingState();
             void ExecuteJumpingState();
             void UpdateAscendingPosition() const;
             void UpdateDescendingPosition() const;
-            void DetermineSpecialJumpingMode();
+            bool ShouldDoAFollowUpJump() const;
 
-			bool ShouldDoAFollowUpJump() const
-			{
-				return (_jump_pattern_index + 2) % 3 == 0;
-			}
-
+            // Debug methods:
             void ChangeStateText(const char* state_string) const;
 
         private:
             SERIALIZE_FIELD(int, state_value);
-			SERIALIZE_FIELD(int, combat_state_value);
-			SERIALIZE_FIELD(bool, second_phase);
+            SERIALIZE_FIELD(int, combat_state_value);
+            SERIALIZE_FIELD(bool, second_phase);
             SERIALIZE_FIELD(GameObject*, hp_bar_go);
             SERIALIZE_FIELD(GameObject*, crystal_target_go);
             SERIALIZE_FIELD(GameObject*, cocoon_placeholder_go);
@@ -254,47 +249,47 @@ namespace Hachiko
             // jumping action carried out by boss. Final value is dependent on
             // _jumping_state and the corresponding delay/duration.
             float _jumping_timer;
-            // Jump pattern when the Jumping mode was set to
-            // JumpingMode::DETERMINED_BY_PATTERN:
+            // Jump pattern is used when the Jumping mode was set to
+            // JumpingMode::DETERMINED_BY_PATTERN on TriggerJumping:
             int _jump_pattern_index;
-			JumpingMode _jumping_pattern[JumpUtil::JUMP_PATTERN_SIZE] = {
-				JumpingMode::BASIC_ATTACK,
-				JumpingMode::BASIC_ATTACK, // 1
-				JumpingMode::STALAGMITE,
-				JumpingMode::BASIC_ATTACK,
-				JumpingMode::BASIC_ATTACK, // 4
-				JumpingMode::WEAPON_CHANGE,
-				JumpingMode::BASIC_ATTACK,
-				JumpingMode::BASIC_ATTACK, // 7
-				JumpingMode::WEAPON_CHANGE,
-				JumpingMode::BASIC_ATTACK,
-				JumpingMode::BASIC_ATTACK, // 10
-				JumpingMode::STALAGMITE,
-				JumpingMode::BASIC_ATTACK,
-				JumpingMode::BASIC_ATTACK, // 13
-				JumpingMode::WEAPON_CHANGE
-			};
+            JumpingMode _jumping_pattern[JumpUtil::JUMP_PATTERN_SIZE] = {
+                JumpingMode::BASIC_ATTACK,
+                JumpingMode::BASIC_ATTACK, // 1
+                JumpingMode::STALAGMITE,
+                JumpingMode::BASIC_ATTACK,
+                JumpingMode::BASIC_ATTACK, // 4
+                JumpingMode::WEAPON_CHANGE,
+                JumpingMode::BASIC_ATTACK,
+                JumpingMode::BASIC_ATTACK, // 7
+                JumpingMode::WEAPON_CHANGE,
+                JumpingMode::BASIC_ATTACK,
+                JumpingMode::BASIC_ATTACK, // 10
+                JumpingMode::STALAGMITE,
+                JumpingMode::BASIC_ATTACK,
+                JumpingMode::BASIC_ATTACK, // 13
+                JumpingMode::WEAPON_CHANGE
+            };
             JumpingMode _current_jumping_mode;
 
-			SERIALIZE_FIELD(GameObject*, _stalagmite_manager_go);
-			StalagmiteManager* _stalagmite_manager;
+            SERIALIZE_FIELD(GameObject*, _stalagmite_manager_go);
+            StalagmiteManager* _stalagmite_manager;
 
             // Debug variables:
             SERIALIZE_FIELD(ComponentText*, _boss_state_text_ui);
 
-			float attack_current_cd;
-			std::vector<Weapon> weapons{};
-			unsigned _current_weapon = 0;
-			unsigned _attack_idx = 0;
-			unsigned _attack_charges = 0;
+            float attack_current_cd;
+            std::vector<Weapon> weapons{};
+            unsigned _current_weapon = 0;
+            unsigned _attack_idx = 0;
+            unsigned _attack_charges = 0;
 
             SERIALIZE_FIELD(GameObject*, _sword_weapon);
             SERIALIZE_FIELD(GameObject*, _claw_weapon);
             SERIALIZE_FIELD(GameObject*, _hammer_weapon);
 
-			float attack_delay_timer = 0.f;
-			float after_attack_wait_timer = 0.f;
-			bool attacked = false;
+            float attack_delay_timer = 0.f;
+            float after_attack_wait_timer = 0.f;
+            bool attacked = false;
 
             bool camera_focus_on_boss = false;
             float time_elapse = 0.0;
@@ -305,11 +300,11 @@ namespace Hachiko
             float enemy_timer = 0.0;
 
 
-			// It needs to start as true so the first normal jump sets it to false
-			bool double_jump_toggle = true;
+            // It needs to start as true so the first normal jump sets it to false
+            bool double_jump_toggle = true;
 
-			const float jump_placeholder_time = 5.f;
-			float jump_placeholder_timer = 0.f;
+            const float jump_placeholder_time = 5.f;
+            float jump_placeholder_timer = 0.f;
         };
     } // namespace Scripting
 } // namespace Hachiko
