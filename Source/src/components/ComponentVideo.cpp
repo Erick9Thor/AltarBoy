79c0b192d94a5ce81e42bc8ae79cc7f8d78b2ecf
miquelmiro3 <miquel.miro3@gmail.com>
2022-10-05 19:24:49 +0200

    Revert "Fix loading video error-part2"
    This reverts commit 954b108941921b6745afce6e595b64ec4885a6bd.

--- Source/src/components/ComponentVideo.cpp
+++ Source/src/components/ComponentVideo.cpp
@@ -174,8 +174,6 @@ void Hachiko::ComponentVideo::Load(const YAML::Node& node)
 
 void Hachiko::ComponentVideo::Preload(unsigned frame_amount)
 {
-    OpenVideo();
-
     preloaded = true;
     preloaded_frames = frame_amount;
     preloaded_frame_textures = new unsigned[frame_amount];
