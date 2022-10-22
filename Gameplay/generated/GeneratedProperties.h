4e04c4695bceda3c32054be58185aa7b98d9d834
Baran Surucu <baransrc@gmail.com>
2022-10-22 21:15:40 +0200

    Undo changes on Gameplay/generated.

--- Gameplay/generated/GeneratedProperties.h
+++ Gameplay/generated/GeneratedProperties.h
@@ -2,21 +2,13 @@
 
 #include "scriptingUtil/framework.h"
 
-extern "C" const GAMEPLAY_API size_t __cdecl SCRIPT_COUNT = 41;
+extern "C" const GAMEPLAY_API size_t __cdecl SCRIPT_COUNT = 27;
 
 extern "C" const GAMEPLAY_API char* __cdecl SCRIPT_NAMES[SCRIPT_COUNT] =
 {
-	"CutsceneManager",
-	"PauseMenu",
 	"TriggerAnim",
-	"BloomAnimator",
-	"BounceEffect",
-	"HoverEffect",
-	"RotationEffect",
 	"Stats",
 	"CrystalExplosion",
-	"Stalagmite",
-	"BossController",
 	"BugAnimationManager",
 	"EnemyController",
 	"CombatManager",
@@ -26,12 +18,9 @@ extern "C" const GAMEPLAY_API char* __cdecl SCRIPT_NAMES[SCRIPT_COUNT] =
 	"PlayerController",
 	"PlayerSoundManager",
 	"RoomTeleporter",
-	"AssetsObstacle",
 	"AudioManager",
 	"BlinkingLight",
-	"BossLaserController",
 	"CameraPosChange",
-	"Centipedes",
 	"CrystalPlatform",
 	"DoorController",
 	"DynamicCamera",
@@ -41,9 +30,6 @@ extern "C" const GAMEPLAY_API char* __cdecl SCRIPT_NAMES[SCRIPT_COUNT] =
 	"LevelManager",
 	"PillarCheckpoint",
 	"Spawner",
-	"StalagmiteManager",
-	"TimeManager",
-	"Tutorial",
 	"BackToMainMenu",
 	"DebugManager",
 	"MainMenuManager"
